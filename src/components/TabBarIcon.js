186631ba273da9c350626e1dae6b11191334136f
Lukas Gjetting <lukasgjetting@gmail.com>
2020-04-22 17:17:03 +0200

    Fix indentation

--- src/components/TabBarIcon.js
+++ src/components/TabBarIcon.js
@@ -2,12 +2,12 @@ import { Ionicons } from '@expo/vector-icons';
 import React from 'react';
 
 const TabBarIcon = ({ name, focused }) => (
-<Ionicons
-  name={name}
-  size={38}
-  style={{ marginBottom: -3 }}
-  color={focused ? '#00F' : '#FF0'}
-/>
+  <Ionicons
+    name={name}
+    size={38}
+    style={{ marginBottom: -3 }}
+    color={focused ? '#00F' : '#FF0'}
+  />
 );
 
 export default TabBarIcon;
